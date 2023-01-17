0c87765801740c0c202d6fcf49a170b5f693d938
sohamtiwari3120-jetson-teledia <soham.tiwari800@gmail.com>
2023-01-17 18:25:08 -0500

    fix: increased time delta resolution  to microseconds

--- ZeroMQ_REQ_client.py
+++ ZeroMQ_REQ_client.py
@@ -1,13 +1,16 @@
 import zmq, datetime, time, json, msgpack
-
+from datetime import timedelta
 base_time = datetime.datetime(1, 1, 1)
+delta = timedelta(microseconds=1)
 def send_payload(pub_sock, topic, message, originatingTime=None):
     payload = {}
     payload[u"message"] = message
     if originatingTime is None:
-        originatingTime = int((datetime.datetime.utcnow() - base_time).total_seconds()) * 1e7
+        originatingTime = (datetime.datetime.utcnow() - base_time)/delta * 1e1
     payload[u"originatingTime"] = originatingTime
+    print(payload)
     pub_sock.send_multipart([topic.encode(), msgpack.dumps(payload)])
+    return originatingTime
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
@@ -17,7 +20,9 @@ socket.connect("tcp://128.2.204.249:40001")
 time.sleep(1)
 
 request = ">>>>> Hello from afar! <<<<<<"
-
+old = 0
+new = 0
+flag = 0
 while True:
 
     # Send the request
@@ -26,9 +31,15 @@ while True:
     payload['originatingTime'] = datetime.datetime.utcnow().isoformat()
     print(f"Sending request: {request}")
     # socket.send_multipart(['fake-topic'.encode(), json.dumps(payload).encode('utf-8')])
-    send_payload(socket, "fake-topic", {"keypoints":[1, 2, 3], "poses": ['sleeping', 'standing']})
+    new = send_payload(socket, "fake-topic", [1, 2, 3])
+    if((new - old)!=0):
+        print(new - old)
+        if flag == 1:
+            break
+        flag = 1
+    old = new
     #  Get the reply
     reply = socket.recv()
     print(f"Received reply: {reply}")
 
-    time.sleep(2)
+    # time.sleep(2)
